29ae8516f273a08ba339b78c847a4b78b7865259
JCLemos92 <ee10201@fe.up.pt>
2025-07-20 01:12:48 +0100

    Initialize state with vehicles

--- src/main.jsx
+++ src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import { AuctionCarList } from './pages/AuctionCarList';
+import { INITIAL_STATE } from './store/initialState';
+import { StoreProvider } from './store/useStore';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <StoreProvider initialValues={INITIAL_STATE}>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<AuctionCarList />} />
         </Routes>
-    </BrowserRouter>
+      </BrowserRouter>
+    </StoreProvider>
   </StrictMode>,
 );
---
+++ src/store/initialState.js
@@ -0,0 +1,5 @@
+import vehicles_dataset from '../../db/vehicles_dataset.json';
+
+export const INITIAL_STATE = {
+  vehicles: vehicles_dataset,
+};
